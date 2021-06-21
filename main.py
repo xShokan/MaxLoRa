4fed3f3b7d04adee0936b1919e384f58fe83b232
shokanxu <shokanxu@tencent.com>
2021-06-21 22:34:37 +0800

    Update

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import SetSFwithOurMethod
 NumArray = [100, 200, 300, 400, 500]
 for i in range(len(NumArray)):
     GenerateData.Generate(NumArray[i], [[100, 200], [100, 200], [200, 300], [200, 300], [300, 400], [300, 400]])
-    SetSFwithOurMethod.SetSF(NumArray[i], 6, 10, 3)
+    SetSFwithOurMethod.SetSF(NumArray[i], 6, 10, 4)
